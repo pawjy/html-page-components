b280b96a8cd4681097c77261c8fae17fcdf57d5e
Wakaba <wakaba@suikawiki.org>
2021-11-05 19:18:26 +0900

    GSI map combinations

--- src/maps.js
+++ src/maps.js
@@ -10,12 +10,141 @@
   }).apply ({});
   var L = console.pcMaps.L = window.L;
   L.noConflict ();
-  
+
   var define = function (def) {
     var e = document.createElementNS ('data:,pc', 'element');
     e.pcDef = def;
     document.head.appendChild (e);
   }; // define
+
+  var noImageURL = 'https://rawgit.com/wakaba/html-page-components/master/css/noimage.svg';
+  // Credit required by GSI.
+  var gsiCreditHTML = "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank' lang=ja>\u56FD\u571F\u5730\u7406\u9662</a>";
+  var gsiPhotoCreditHTML = "<details is=pc-map-credit-details><summary lang=ja>\u56FD\u571F\u5730\u7406\u9662\u4ED6</summary><p>"+gsiCreditHTML+"<p lang=ja>\u30C7\u30FC\u30BF\u30BD\u30FC\u30B9\uFF1ALandsat8\u753B\u50CF\uFF08GSI,TSIC,GEO Grid/AIST\uFF09, Landsat8\u753B\u50CF\uFF08courtesy of the U.S. Geological Survey\uFF09, \u6D77\u5E95\u5730\u5F62\uFF08GEBCO\uFF09<p lang=en>Images on \u4E16\u754C\u885B\u661F\u30E2\u30B6\u30A4\u30AF\u753B\u50CF obtained from site <a href=https://lpdaac.usgs.gov/data_access target=_blank>https://lpdaac.usgs.gov/data_access</a> maintained by the NASA Land Processes Distributed Active Archive Center (LP DAAC), USGS/Earth Resources Observation and Science (EROS) Center, Sioux Falls, South Dakota, (Year). Source of image data product.</details>";
+  
+  var ColorOpacities = console.pcMaps.ColorOpacities = {
+    '0,0,0': 1,
+    '30,30,30': 1,
+    '50,50,50': 1,
+    '80,80,80': 1,
+    '100,100,100': 1,
+    '200,200,200': 0.05,
+    '213,213,213': 0.02,
+    '233,233,233': 0,
+    '255,255,255': 0,
+    '0,90,60': 1,
+    '94,189,108': 0.4,
+    '190,210,255': 0.1,
+    '105,150,255': 0.2,
+    '20,90,255': 0.2,
+    '96,25,133': 1,
+    '253,134,75': 0.2,
+    '199,136,137': 0.4,
+    '219,199,149': 0.001,
+    '203,165,72': 0.001,
+    '216,194,137': 0.001,
+    '251,221,206': 0.001,
+    '255,255,230': 0,
+    '255,255,0': 0.4,
+    '255,230,190': 0.4,
+  }; // ColorOpacities
+  var PaletteColors = Object.keys (ColorOpacities).map (_ => _.split (/,/));
+  
+  console.pcMaps.printPaletteColors = () => {
+    var l = document.createElement ('ul');
+    PaletteColors.forEach (pRGB => {
+      var li = document.createElement ('li');
+      var c = document.createElement ('span');
+      c.textContent = c.style.background = 'rgb(' + pRGB + ')';
+      li.appendChild (c);
+      li.appendChild (document.createTextNode (' : '));
+      var op = document.createElement ('span');
+      op.style.background = 'rgb(' + pRGB + ')';
+      op.textContent = op.style.opacity = ColorOpacities[pRGB];
+      li.appendChild (op);
+      l.appendChild (li);
+    });
+    document.body.appendChild (l);
+    l.scrollIntoView ();
+  }; // console.pcMaps.printPaletteColors
+
+  function ddRGB ([r1, g1, b1], [r2, g2, b2]) {
+    var r = r1-r2;
+    var g = g1-g2;
+    var b = b1-b2;
+    return r*r*3 + g*g*5 + b*b;
+  } // ddRGB
+  
+  function computeColorOpacities (colors) {
+    Object.keys (colors).map (key => {
+      var rgb = colors[key];
+
+      var minRGB = null;
+      var minDD = Infinity;
+      PaletteColors.forEach (pRGB => {
+        var dd = ddRGB (pRGB, rgb);
+        if (dd < minDD) {
+          minDD = dd;
+          minRGB = pRGB;
+        }
+      });
+      
+      ColorOpacities[rgb] = ColorOpacities[minRGB];
+    }); // colors
+  } // computeColorOpacities
+
+  L.GridLayer.GSIOverlay = L.GridLayer.extend ({
+    createTile: function (coords, done) {
+      var canvas = document.createElement ('canvas');
+      var tileSize = this.getTileSize ();
+      canvas.width = tileSize.x;
+      canvas.height = tileSize.y;
+      var ctx = canvas.getContext ('2d');
+      
+      var img = document.createElement ('img');
+      new Promise ((ok, ng) => {
+        img.setAttribute ('crossorigin', '');
+        var u = 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png';
+        img.src = u.replace (/\{x\}/, coords.x)
+                   .replace (/\{y\}/, coords.y)
+                   .replace (/\{z\}/, coords.z);
+        img.onload = ok;
+        img.onerror = ng
+      }).then (() => {
+        ctx.drawImage (img, 0, 0, img.naturalWidth, img.naturalHeight);
+
+        var id = ctx.getImageData (0, 0, canvas.width, canvas.height);
+        var colors = {}; // {(r,g,b): (# of pixels), ...}
+        for (var i = 0; i < id.data.byteLength; i+=4) {
+          var rgb = [id.data[i], id.data[i+1], id.data[i+2]];
+          if (ColorOpacities[rgb] === undefined) colors[rgb] = rgb;
+        }
+        computeColorOpacities (colors);
+        for (var i = 0; i < id.data.byteLength; i+=4) {
+          var rgb = [id.data[i], id.data[i+1], id.data[i+2]];
+          id.data[i+3] = 0xFF * ColorOpacities[rgb];
+        }
+        ctx.putImageData (id, 0, 0);
+        
+        done (null, canvas);
+      }, err => { // err is img.onerror's error
+        var img = document.createElement ('img');
+        return new Promise ((ok, ng) => {
+          img.setAttribute ('crossorigin', '');
+          img.src = this.options.errorTileUrl;
+          img.onload = ok;
+          img.onerror = ng
+        }).then (() => {
+          ctx.drawImage (img, 0, 0, canvas.width, canvas.height);
+        });
+      });
+
+      return canvas;
+    }, // createTile
+  }); // L.GridLayer.GSIOverlay
+  L.gridLayer.gsiOverlay = function (opts) {
+    return new L.GridLayer.GSIOverlay (opts);
+  };
 
   var gmPromise;
   var loadGoogleMaps = () => {
@@ -39,10 +168,6 @@
     });
   }; // loadGoogleMaps
 
-  var noImageURL = 'https://rawgit.com/wakaba/html-page-components/master/css/noimage.svg';
-  // Credit required by GSI.
-  var gsiCreditHTML = "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank' lang=ja>\u56FD\u571F\u5730\u7406\u9662</a>";
-  
   define ({
     name: 'map-area',
     props: {
@@ -418,7 +543,7 @@
         } else if (type === 'gsi-photo') {
           var lPhoto = L.tileLayer
               ('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg', {
-                attribution: "<details is=pc-map-credit-details><summary lang=ja>\u56FD\u571F\u5730\u7406\u9662\u4ED6</summary><p>"+gsiCreditHTML+"<p lang=ja>\u30C7\u30FC\u30BF\u30BD\u30FC\u30B9\uFF1ALandsat8\u753B\u50CF\uFF08GSI,TSIC,GEO Grid/AIST\uFF09, Landsat8\u753B\u50CF\uFF08courtesy of the U.S. Geological Survey\uFF09, \u6D77\u5E95\u5730\u5F62\uFF08GEBCO\uFF09<p lang=en>Images on \u4E16\u754C\u885B\u661F\u30E2\u30B6\u30A4\u30AF\u753B\u50CF obtained from site <a href=https://lpdaac.usgs.gov/data_access target=_blank>https://lpdaac.usgs.gov/data_access</a> maintained by the NASA Land Processes Distributed Active Archive Center (LP DAAC), USGS/Earth Resources Observation and Science (EROS) Center, Sioux Falls, South Dakota, (Year). Source of image data product.</details>",
+                attribution: gsiPhotoCreditHTML,
                 errorTileUrl,
                 maxNativeZoom: 18,
                 minNativeZoom: 2,
@@ -443,6 +568,39 @@
                 opacity: 0.8,
               });
           layers.push (lStd);
+        } else if (type === 'gsi-hillshade-standard') {
+          var lGSI = L.gridLayer.gsiOverlay ({
+            //attribution: gsiCreditHTML,
+            errorTileUrl,
+            maxNativeZoom: 18,
+            minNativeZoom: 2,
+          });
+          var lShade = L.tileLayer
+              ('https://cyberjapandata.gsi.go.jp/xyz/hillshademap/{z}/{x}/{y}.png', {
+                attribution: gsiCreditHTML,
+                errorTileUrl,
+                maxNativeZoom: 16,
+                minNativeZoom: 2,
+                opacity: 0.6,
+              });
+          layers.push (lShade);
+          layers.push (lGSI);
+        } else if (type === 'gsi-photo-standard') {
+          var lGSI = L.gridLayer.gsiOverlay ({
+            //attribution: gsiCreditHTML,
+            errorTileUrl,
+            maxNativeZoom: 18,
+            minNativeZoom: 2,
+          });
+          var lPhoto = L.tileLayer
+              ('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg', {
+                attribution: gsiPhotoCreditHTML,
+                errorTileUrl,
+                maxNativeZoom: 18,
+                minNativeZoom: 2,
+             });
+          layers.push (lPhoto);
+          layers.push (lGSI);
         }
         
         map.eachLayer (l => map.removeLayer (l));
@@ -454,3 +612,48 @@
   }); // <map-area>
 
 }) ();
+
+/*
+
+Copyright 2017-2021 Wakaba <wakaba@suikawiki.org>.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Affero General Public License for more details.
+
+You does not have received a copy of the GNU Affero General Public
+License along with this program, see <https://www.gnu.org/licenses/>.
+
+*/
+/* Leaflet */
+/*
+Copyright (c) 2010-2021, Vladimir Agafonkin
+Copyright (c) 2010-2011, CloudMade
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
